cbe1bd69369835bd5b84e6df48ff484e0dcf6ed6
tanvir35web <islammdtanvirul@gmail.com>
2024-01-08 17:11:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const [inputText, setInputText] = useState({
+    firstName: "Tanvirul",
+    lastName: "Islam",
+    email: "tanvir.niter09@gmail.com",
+  });
+
+  return (
+    <>
+      <label>
+        First Name:
+        <input
+          value={inputText.firstName}
+          onChange={(e) => {
+            setInputText({
+              ...inputText,
+              firstName: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <br />
+
+      <label>
+        Last Name:
+        <input
+          value={inputText.lastName}
+          onChange={(e) => {
+            setInputText({
+              ...inputText,
+              lastName: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <br />
+
+      <label>
+        Email:
+        <input
+          value={inputText.email}
+          onChange={(e) => {
+            setInputText({
+              ...inputText,
+              email: e.target.value,
+            });
+          }}
+        />
+      </label>
+
+      <p>
+        {inputText.firstName} {inputText.lastName} {inputText.email}
+      </p>
+    </>
+  );
+}
